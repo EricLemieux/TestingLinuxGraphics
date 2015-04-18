2a627b07089fb07c4a16a6405fcc993f3fc8af92
Eric Lemieux <ericlemieux2@gmail.com>
2015-04-18 11:02:42 -0400

    Using the correct OpenGL version

---
+++ CMakeFiles/TestingLinuxGraphics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestingLinuxGraphics.dir/src/main.cpp.o"
+  "TestingLinuxGraphics.pdb"
+  "TestingLinuxGraphics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestingLinuxGraphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/gl.h>
 #include <SDL2/SDL.h>
 #undef main
 
 int main()
 {
+    std::cout<<"Create Window\n";
+
     SDL_Window* window;
     SDL_GLContext context;
 
@@ -14,11 +18,19 @@ int main()
         return 1;
     }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
     window = SDL_CreateWindow("Hello World",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
     context = SDL_GL_CreateContext(window);
+
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if(err != GLEW_OK)
+    {
+        std::cout<<"Error with glew: "<<glewGetErrorString(err) <<"\n";
+    }
+    
 
     SDL_GL_MakeCurrent(window, context);
     SDL_GL_SetSwapInterval(1);
@@ -27,6 +39,8 @@ int main()
 
     SDL_Event event;
 
+    std::cout<<"OpenGL Version: "<<glGetString(GL_VERSION)<<"\n";
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
     while(gameLoop)
     {
         if(SDL_PollEvent(&event))
@@ -36,6 +50,9 @@ int main()
                 gameLoop = false;
             }
         }
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	SDL_GL_SwapWindow(window);
     }
 
     //Cleanup
